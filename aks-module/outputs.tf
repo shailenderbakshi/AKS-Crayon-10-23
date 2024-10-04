ea545c076e9185fa5e321b77e4a23da444e48d87
shailender singh <shailender_bakshi@yahoo.com>
2024-10-04 15:57:45 +0000

    Updated outputs.tf

---
+++ aks-module/outputs.tf
@@ -0,0 +1,8 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  sensitive = true
+}
