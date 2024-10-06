a4eeb55b7bd90cf9b651c7e33a93a0ce499640a6
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 14:00:36 +0000

    Updated variables.tf

--- terraform-project/modules/aks/variables.tf
+++ terraform-project/modules/aks/variables.tf
@@ -27,3 +27,48 @@ variable "default_node_pool_count" {
   description = "The number of nodes in the default node pool"
   type        = number
   default     = 3
+}
+
+variable "default_node_pool_vm_size" {
+  description = "The size of the virtual machines in the default node pool"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+variable "subnet_id" {
+  description = "The subnet ID to deploy the AKS cluster into"
+  type        = string
+}
+
+variable "service_cidr" {
+  description = "The service CIDR for the AKS cluster"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "dns_service_ip" {
+  description = "The IP address of the DNS service within the AKS cluster"
+  type        = string
+  default     = "10.0.0.10"
+}
+
+variable "docker_bridge_cidr" {
+  description = "The Docker bridge CIDR for the AKS cluster"
+  type        = string
+  default     = "172.17.0.1/16"
+}
+
+variable "admin_group_object_id" {
+  description = "The object ID of the AAD group with cluster admin permissions"
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to the AKS cluster"
+  type        = map(string)
+}
