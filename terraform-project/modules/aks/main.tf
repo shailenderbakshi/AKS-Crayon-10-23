1d7063b72d883655d1a151256ed8d961a2913c56
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 11:53:15 +0000

    Updated main.tf

--- terraform-project/modules/aks/main.tf
+++ terraform-project/modules/aks/main.tf
@@ -1,24 +1,42 @@
-# AKS Cluster Module
-module "aks" {
-  source = "./modules/aks"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
 
-  aks_name                  = var.aks_name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  dns_prefix                = var.dns_prefix
-  default_node_pool_name    = var.default_node_pool_name
-  default_node_pool_count   = var.default_node_pool_count
-  default_node_pool_vm_size = var.default_node_pool_vm_size
-  subnet_id                 = module.vnet.subnet_id
-  service_cidr              = var.service_cidr
-  dns_service_ip            = var.dns_service_ip
-  docker_bridge_cidr        = var.docker_bridge_cidr
-  admin_group_object_id     = var.admin_group_object_id
+  default_node_pool {
+    name       = var.default_node_pool_name
+    node_count = var.default_node_pool_count
+    vm_size    = var.default_node_pool_vm_size
+    vnet_subnet_id = var.subnet_id
+  }
 
-  # Dynamically pass Log Analytics Workspace ID
-  log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
-  environment                = var.environment
+  identity {
+    type = "SystemAssigned"
+  }
 
-  # Add explicit dependency on the Log Analytics module
-  depends_on = [module.log_analytics]
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    service_cidr      = var.service_cidr
+    dns_service_ip    = var.dns_service_ip
+    docker_bridge_cidr = var.docker_bridge_cidr
+  }
+
+  role_based_access_control {
+    enabled = true
+    azure_active_directory {
+      managed = true
+      admin_group_object_ids = [var.admin_group_object_id]
+    }
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled = true
+      log_analytics_workspace_id = var.log_analytics_workspace_id
+    }
+  }
+
+  tags = var.tags
 }
