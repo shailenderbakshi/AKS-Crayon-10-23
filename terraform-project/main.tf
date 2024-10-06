2083783c97687691e5cd3d09449535c322a3f3cf
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 12:08:47 +0000

    Updated main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -1,3 +1,37 @@
+# Resource Group and Storage Account Module
+module "rg_storage" {
+  source               = "./modules/rg_storage"
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  storage_account_name = var.storage_account_name
+  account_tier         = var.account_tier
+  replication_type     = var.replication_type
+}
+
+# Virtual Network (VNet) Module
+module "vnet" {
+  source               = "./modules/vnet"
+  vnet_name            = var.vnet_name
+  vnet_address_space   = var.vnet_address_space
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  subnet_name          = var.subnet_name
+  subnet_address_prefix = var.subnet_address_prefix
+}
+
+# Log Analytics Module
+module "log_analytics" {
+  source = "./modules/log_analytics"
+
+  log_analytics_workspace_name = var.log_analytics_workspace_name
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  sku                          = var.log_analytics_sku
+  retention_in_days            = var.log_analytics_retention
+  tags                         = var.tags
+}
+
+# AKS Module
 module "aks" {
   source = "./modules/aks"
 
@@ -8,7 +42,7 @@ module "aks" {
   default_node_pool_name    = var.default_node_pool_name
   default_node_pool_count   = var.default_node_pool_count
   default_node_pool_vm_size = var.default_node_pool_vm_size
-  subnet_id                 = module.vnet.subnet_id
+  subnet_id                 = module.vnet.subnet_id  # Reference to VNet subnet
   service_cidr              = var.service_cidr
   dns_service_ip            = var.dns_service_ip
   docker_bridge_cidr        = var.docker_bridge_cidr
