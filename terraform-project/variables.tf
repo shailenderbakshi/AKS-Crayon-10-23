4bfaf5fadcf09b34a705369cdf5394c2288ad12d
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 09:26:54 +0000

    Updated variables.tf

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,27 +1,46 @@
+# Variables for Resource Group and Storage Account
 variable "resource_group_name" {
-  description = "Name of the resource group"
+  description = "The name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "Location of the resource group"
+  description = "The location of the resource group"
   type        = string
-  default     = "East US"
 }
 
 variable "storage_account_name" {
-  description = "Name of the storage account"
+  description = "The name of the storage account"
   type        = string
 }
 
 variable "account_tier" {
-  description = "Tier of the storage account"
+  description = "The account tier of the storage account"
   type        = string
-  default     = "Standard"
 }
 
 variable "replication_type" {
-  description = "Replication type of the storage account"
+  description = "The replication type of the storage account"
   type        = string
-  default     = "LRS"
+}
+
+# Variables for Virtual Network
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "The address prefix for the subnet"
+  type        = string
 }
