6c02141119d5173bc17ec549a6061b4e559697c1
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 09:34:03 +0000

    Updated main.tf

--- terraform-project/modules/vnet/main.tf
+++ terraform-project/modules/vnet/main.tf
@@ -1,11 +1,10 @@
-resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  subnet {
-    name           = var.subnet_name
-    address_prefix = var.subnet_address_prefix
-  }
+# Virtual Network module
+module "vnet" {
+  source               = "./modules/vnet"
+  vnet_name            = var.vnet_name
+  vnet_address_space   = var.vnet_address_space
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  subnet_name          = var.subnet_name
+  subnet_address_prefix = var.subnet_address_prefix
 }
