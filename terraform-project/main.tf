ae52d6bb12acaebc087cf236ba9e6be3d4a69381
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 12:53:33 +0000

    Updated main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -18,3 +18,15 @@ module "vnet" {
   subnet_name          = var.subnet_name
   subnet_address_prefix = var.subnet_address_prefix
 }
+
+# Log Analytics Module
+module "log_analytics" {
+  source = "./modules/log_analytics"
+
+  log_analytics_workspace_name = var.log_analytics_workspace_name
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  sku                          = var.log_analytics_sku
+  retention_in_days            = var.log_analytics_retention
+  tags                         = var.tags
+}
