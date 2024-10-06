5941d618e7b8240c673680db97fc72f1d20c7fdd
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 10:57:16 +0000

    Added variables.tf

---
+++ terraform-project/modules/aks/variables.tf
@@ -0,0 +1,69 @@
+variable "aks_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the AKS cluster"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "The DNS prefix for the AKS cluster"
+  type        = string
+}
+
+variable "default_node_pool_name" {
+  description = "The default node pool name"
+  type        = string
+}
+
+variable "default_node_pool_count" {
+  description = "The number of nodes in the default pool"
+  type        = number
+}
+
+variable "default_node_pool_vm_size" {
+  description = "The VM size for the default node pool"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where AKS will be deployed"
+  type        = string
+}
+
+variable "service_cidr" {
+  description = "The service CIDR for the AKS cluster"
+  type        = string
+}
+
+variable "dns_service_ip" {
+  description = "The IP address for the DNS service"
+  type        = string
+}
+
+variable "docker_bridge_cidr" {
+  description = "The CIDR for Docker bridge network"
+  type        = string
+}
+
+variable "admin_group_object_id" {
+  description = "Azure AD group object ID for AKS administrators"
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment tag for the cluster"
+  type        = string
+}
