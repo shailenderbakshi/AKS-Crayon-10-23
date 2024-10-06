58719e9dbd933c9055e31a655cf5bff3f172e513
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 09:27:11 +0000

    Updated outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -1,7 +1,21 @@
-output "resource_group_name" {
-  value = module.rg_storage.resource_group_name
+# Outputs for Resource Group and Storage Account
+output "storage_account_id" {
+  description = "The ID of the storage account"
+  value       = module.rg_storage.storage_account_id
 }
 
-output "storage_account_name" {
-  value = module.rg_storage.storage_account_name
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = module.rg_storage.resource_group_name
+}
+
+# Outputs for Virtual Network
+output "vnet_id" {
+  description = "The ID of the virtual network"
+  value       = module.vnet.vnet_id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = module.vnet.subnet_id
 }
