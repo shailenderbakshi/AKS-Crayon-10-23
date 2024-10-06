67dbcb64e38aeb41b243a2139dd793807d6d7071
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 05:09:24 +0000

    Updated outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -1,8 +1,7 @@
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
+output "resource_group_name" {
+  value = module.rg_storage.resource_group_name
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-  sensitive = true
+output "storage_account_name" {
+  value = module.rg_storage.storage_account_name
 }
