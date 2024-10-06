080b1728ee865572068f0f3801f843aadc000bb6
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 05:08:57 +0000

    Updated variables.tf

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,56 +1,27 @@
 variable "resource_group_name" {
-  type        = string
   description = "Name of the resource group"
+  type        = string
 }
 
 variable "location" {
+  description = "Location of the resource group"
   type        = string
-  description = "Location of the AKS cluster"
-}
-
-variable "aks_cluster_name" {
-  type        = string
-  description = "Name of the AKS cluster"
-}
-
-variable "dns_prefix" {
-  type        = string
-  description = "DNS prefix for the AKS cluster"
-}
-
-variable "agent_count" {
-  type        = number
-  description = "Number of AKS agent nodes"
-  default     = 3
-}
-
-variable "vm_size" {
-  type        = string
-  description = "VM size for AKS agent nodes"
-  default     = "Standard_DS2_v2"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags for the resources"
-  default     = {
-    environment = "dev"
-    costcenter  = "12345"
-  }
-}
-
-# Backend storage details for storing Terraform state
-variable "storage_rg_name" {
-  type        = string
-  description = "Resource group for the storage account"
+  default     = "East US"
 }
 
 variable "storage_account_name" {
-  type        = string
   description = "Name of the storage account"
+  type        = string
 }
 
-variable "container_name" {
+variable "account_tier" {
+  description = "Tier of the storage account"
   type        = string
-  description = "Name of the container to store the Terraform state"
+  default     = "Standard"
+}
+
+variable "replication_type" {
+  description = "Replication type of the storage account"
+  type        = string
+  default     = "LRS"
 }
