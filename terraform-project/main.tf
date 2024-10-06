78599e4f860b8d8fdaff097f22d7974676c715b0
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 09:35:36 +0000

    Updated main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -12,7 +12,7 @@ module "rg_storage" {
 module "vnet" {
   source               = "./modules/vnet"
   vnet_name            = var.vnet_name
-  address_space        = var.vnet_address_space
+  vnet_address_space   = var.vnet_address_space
   location             = var.location
   resource_group_name  = var.resource_group_name
   subnet_name          = var.subnet_name
