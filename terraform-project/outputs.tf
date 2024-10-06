2c773f397d4e3f6f7d3b2a762858999ed9ee2067
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 13:30:37 +0000

    Updated outputs.tf

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -1,21 +1,51 @@
-# Outputs for Resource Group and Storage Account
-output "storage_account_id" {
-  description = "The ID of the storage account"
-  value       = module.rg_storage.storage_account_id
+# AKS Cluster Variables
+variable "aks_name" {
+  description = "The name of the AKS cluster"
+  type        = string
 }
 
-output "resource_group_name" {
-  description = "The name of the resource group"
-  value       = module.rg_storage.resource_group_name
+variable "dns_prefix" {
+  description = "DNS prefix for the AKS cluster"
+  type        = string
 }
 
-# Outputs for Virtual Network
-output "vnet_id" {
-  description = "The ID of the virtual network"
-  value       = module.vnet.vnet_id
+variable "default_node_pool_name" {
+  description = "The name of the default node pool"
+  type        = string
+  default     = "default"
 }
 
-output "subnet_id" {
-  description = "The ID of the subnet"
-  value       = module.vnet.subnet_id
+variable "default_node_pool_count" {
+  description = "The number of nodes in the default node pool"
+  type        = number
+  default     = 3
+}
+
+variable "default_node_pool_vm_size" {
+  description = "The size of the virtual machines in the default node pool"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+variable "service_cidr" {
+  description = "The service CIDR for the AKS cluster"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "dns_service_ip" {
+  description = "The IP address of the DNS service within the AKS cluster"
+  type        = string
+  default     = "10.0.0.10"
+}
+
+variable "docker_bridge_cidr" {
+  description = "The Docker bridge CIDR for the AKS cluster"
+  type        = string
+  default     = "172.17.0.1/16"
+}
+
+variable "admin_group_object_id" {
+  description = "The object ID of the AAD group with cluster admin permissions"
+  type        = string
 }
