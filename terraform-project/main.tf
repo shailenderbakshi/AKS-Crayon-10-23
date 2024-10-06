70d1c660600407692aabc27552340c4b4a92f824
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 12:37:26 +0000

    Updated main.tf

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -31,27 +31,27 @@ module "log_analytics" {
   tags                         = var.tags
 }
 
-# AKS Module
-module "aks" {
-  source = "./modules/aks"
+// # AKS Module
+// module "aks" {
+//   source = "./modules/aks"
 
-  aks_name                  = var.aks_name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  dns_prefix                = var.dns_prefix
-  default_node_pool_name    = var.default_node_pool_name
-  default_node_pool_count   = var.default_node_pool_count
-  default_node_pool_vm_size = var.default_node_pool_vm_size
-  subnet_id                 = module.vnet.subnet_id  # Reference to VNet subnet
-  service_cidr              = var.service_cidr
-  dns_service_ip            = var.dns_service_ip
-  docker_bridge_cidr        = var.docker_bridge_cidr
-  admin_group_object_id     = var.admin_group_object_id
+//   aks_name                  = var.aks_name
+//   location                  = var.location
+//   resource_group_name       = var.resource_group_name
+//   dns_prefix                = var.dns_prefix
+//   default_node_pool_name    = var.default_node_pool_name
+//   default_node_pool_count   = var.default_node_pool_count
+//   default_node_pool_vm_size = var.default_node_pool_vm_size
+//   subnet_id                 = module.vnet.subnet_id  # Reference to VNet subnet
+//   service_cidr              = var.service_cidr
+//   dns_service_ip            = var.dns_service_ip
+//   docker_bridge_cidr        = var.docker_bridge_cidr
+//   admin_group_object_id     = var.admin_group_object_id
 
-  # Dynamically pass Log Analytics Workspace ID
-  log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
-  environment                = var.environment
+//   # Dynamically pass Log Analytics Workspace ID
+//   log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
+//   environment                = var.environment
 
-  # Add explicit dependency on the Log Analytics module
-  depends_on = [module.log_analytics]
-}
+//   # Add explicit dependency on the Log Analytics module
+//   depends_on = [module.log_analytics]
+// }
