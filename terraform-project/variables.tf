eea9c9beda36287a07498ca7d4163bcfda215419
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 13:02:28 +0000

    Updated variables.tf

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,12 +1,34 @@
-# Variables for Resource Group and Storage Account
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "The location of the resource group"
+  description = "The location where resources will be created"
   type        = string
+}
+
+variable "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics workspace"
+  type        = string
+}
+
+variable "log_analytics_sku" {
+  description = "The SKU for the Log Analytics workspace (e.g., PerGB2018)"
+  type        = string
+  default     = "PerGB2018"
+}
+
+variable "log_analytics_retention" {
+  description = "The retention period for logs in days"
+  type        = number
+  default     = 30
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources"
+  type        = map(string)
+  default     = {}
 }
 
 variable "storage_account_name" {
@@ -15,23 +37,22 @@ variable "storage_account_name" {
 }
 
 variable "account_tier" {
-  description = "The account tier of the storage account"
+  description = "The account tier for the storage account (e.g., Standard or Premium)"
   type        = string
 }
 
 variable "replication_type" {
-  description = "The replication type of the storage account"
+  description = "The replication type for the storage account (e.g., LRS, GRS)"
   type        = string
 }
 
-# Variables for Virtual Network
 variable "vnet_name" {
   description = "The name of the virtual network"
   type        = string
 }
 
 variable "vnet_address_space" {
-  description = "The address space of the virtual network"
+  description = "The address space for the virtual network"
   type        = list(string)
 }
 
