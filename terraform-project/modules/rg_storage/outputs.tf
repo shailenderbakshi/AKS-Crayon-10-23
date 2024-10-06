205667a3af448c1a0059cd3c254185fe6b46a001
shailender singh <shailender_bakshi@yahoo.com>
2024-10-06 09:42:00 +0000

    Updated outputs.tf

--- terraform-project/modules/rg_storage/outputs.tf
+++ terraform-project/modules/rg_storage/outputs.tf
@@ -5,3 +5,8 @@ output "resource_group_name" {
 output "storage_account_name" {
   value = azurerm_storage_account.storage.name
 }
+
+output "storage_account_id" {
+  description = "The ID of the storage account"
+  value       = azurerm_storage_account.main.id
+}
