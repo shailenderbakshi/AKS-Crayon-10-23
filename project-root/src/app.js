d9f4ef610d17b62d91c25731ca39ce66e7e48571
shailender singh <shailender_bakshi@yahoo.com>
2024-10-16 15:19:03 +0000

    Deleted src

--- project-root/src/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8081;  // Updated to default to 8081
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
