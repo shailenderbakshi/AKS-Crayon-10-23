293330dd420eeeaeb623bbb52f2d616a8dd33e31
Shailender Singh <50791698+shailenderbakshi@users.noreply.github.com>
2024-10-23 05:11:34 +0100

    Update provider.tf

--- terraform-project/provider.tf
+++ terraform-project/provider.tf
@@ -1,12 +1,16 @@
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "myStorageResourceGroup"
-    storage_account_name  = "terracount02"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "myStorageResourceGroup"
+    storage_account_name = "terracount0203"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
