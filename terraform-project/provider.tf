a49ee0a02f8e8cdf5947e1a9a8ebd62732c3de93
Shailender Singh <50791698+shailenderbakshi@users.noreply.github.com>
2024-10-23 10:52:55 +0100

    Update provider.tf

--- terraform-project/provider.tf
+++ terraform-project/provider.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name   = "myStorageResourceGroup"
-    storage_account_name  = "terracount02"
+    storage_account_name  = "terracount0203"
     container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    key                   = "aks-terraform.tfstate"
   }
 }
